991d780d6b5a6df58e891f197f2d034721ecfedb
Raevsky Ra <kaneky.ky@outlook.com>
2022-09-11 20:36:23 +0300

    0.1

---
+++ ReviewerWindowsSetUpWPF/MainWindow.xaml.cs
@@ -0,0 +1,49 @@
+﻿using Microsoft.Win32;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Management;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace ReviewerWindowsSetUpWPF
+{
+    /// <summary>
+    /// Логика взаимодействия для MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+        private void btClose(object sender, RoutedEventArgs e)
+        {
+            Close();
+        }
+
+        private void btCollapse(object sender, RoutedEventArgs e)
+        {
+            this.WindowState = WindowState.Minimized;
+        }
+
+
+        private void Grid_Loaded(object sender, RoutedEventArgs e)
+        {
+            RegistryKey key = Registry.LocalMachine.OpenSubKey(@"HARDWARE\DESCRIPTION\System\CentralProcessor\0");
+            Processor.Text = key.GetValue("ProcessorNameString").ToString();
+
+        }
+    }
+}
